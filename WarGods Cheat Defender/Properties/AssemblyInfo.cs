9c2f562b801846dfe2d4dccd120ee2e77817b0ea
BulgPride <Dekryptor@users.noreply.github.com>
2022-11-06 19:08:34 +0200

    Create AssemblyInfo.cs

---
+++ WarGods Cheat Defender/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("2.4.0.4")]
+[assembly: AssemblyTrademark("www.wargods.ro")]
+[assembly: AssemblyCopyright("Tiger @ wGods")]
+[assembly: AssemblyProduct("WarGods Cheat Defender")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyFileVersion("2.4.0.4")]
+[assembly: Guid("92505951-2e5c-49e3-81dc-1915fce80c85")]
+[assembly: AssemblyTitle("WarGods Cheat Defender")]
+[assembly: AssemblyCompany("WarGods.ro")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("WarGods Cheat Defender")]
